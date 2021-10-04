3f708b7a84b0b1180d3a86ce5899dc544d843950
grace-ko <gracekodes@gmail.com>
2021-10-04 13:14:25 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+const orgID = {
+  ongrid: '06277C99-00C9-23EB-FD08FE5275BCC0C5',
+  speedsf: '072A885E-AD68-6F64-E88C19E4D0D21DFB',
+  hod: '38E4A131-AB02-6D17-AB34547979B28DE5'
+}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+    for (const prop in orgID) {
+      axios.get(`https://api.motorsportreg.com/rest/calendars/organization/${orgID[prop]}.json`)
+        .then(res => {
+          console.log(res.data.response.events)
+        })
+    }
+  }
+
+
+  render() {
+    return 'hello';
+  }
 }
 
 export default App;
